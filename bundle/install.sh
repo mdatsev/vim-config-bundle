f88d2112ce7c23fad0db7979839219adeed00620
mdatsev <martin.datsev.p@gmail.com>
2019-08-12 11:13:40 +0300

    update dependancies, use npx

--- bundle/install.sh
+++ bundle/install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Install eslint and prettify npm dependancies
-npm install -g \
-    eslint@5.x \
-    eslint-config-standard@12.x \
-    eslint-plugin-import@2.x \
-    eslint-plugin-jest@22.x \
-    eslint-plugin-no-parameter-e@0.x \
-    eslint-plugin-node@9.x \
-    eslint-plugin-pep8-blank-lines@0.x \
-    eslint-plugin-promise@4.x \
-    eslint-plugin-standard@4.x \
-    prettier@1.x
+# npm install -g \
+#     eslint@5.x \
+#     eslint-config-standard@12.x \
+#     eslint-plugin-import@2.x \
+#     eslint-plugin-jest@22.x \
+#     eslint-plugin-node@9.x \
+#     eslint-plugin-promise@4.x \
+#     eslint-plugin-standard@4.x \
+#     eslint-plugin-unicorn@9.x \
+#     prettier@1.x
 
 # Install Pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
